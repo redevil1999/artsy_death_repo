0f4515b2976ab1de8e3c13f84ba1c4f5a1cf0a3d
RobinDevillers <re.devillers1999@gmail.com>
2020-01-20 13:31:49 +0100

    created a csv file out of all the jsons

--- musical_artist_death.py
+++ musical_artist_death.py
@@ -1,38 +1,31 @@
-# import json
+import json
+import os
 
-# with open('filtered_music.json', encoding= 'utf8') as file:
-#     data_musical_artist = json.load(file)
+musical_artist = []
 
-# musical_artist = []
+for filename in os.listdir('People'):
+    with open(f'People/{filename}', encoding= 'utf8') as file:
+        data_musical_artist = json.load(file)
+        
 
-# for artist in data_musical_artist:
-#     if artist['musical artist']:
-#         musical_artist.append(artist)
-#     elif artist['MusicalArtist']:
-#         musical_artist.append(artist)
-#     elif artist['musicalArtist']:
-#         musical_artist.append(artist)
-#     elif artist['MusicGroup']:
-#         musical_artist.append(artist)
-#     elif artist['musician']:
-#         musical_artist.append(artist)
+        for artist in data_musical_artist:
+            if "musical artist" in artist['http://www.w3.org/1999/02/22-rdf-syntax-ns#type_label']:
+                musical_artist.append(artist)
+            elif "http://dbpedia.org/ontology/MusicalArtist" in artist['http://www.w3.org/1999/02/22-rdf-syntax-ns#type']:
+                musical_artist.append(artist)
+            elif "MusicGroup" in artist['http://www.w3.org/1999/02/22-rdf-syntax-ns#type_label']:
+                musical_artist.append(artist)
+            elif 'http://purl.org/dc/elements/1.1/description' in artist and "musician" in artist['http://purl.org/dc/elements/1.1/description']:
+                musical_artist.append(artist)
+            elif 'ontology/associatedBand_label' in artist:
+                musical_artist.append(artist)
+            elif 'ontology/associatedBand' in artist:
+                musical_artist.append(artist)
 
-# musical_artist_age = []
 
-# for artist in musical_artist_selected:
-#     if artist['ontology/birthYear'] >= 1915:
-#         musical_artist_age.append(artist)
-#         musical_artist_age.append(artist['ontology/deathYear'])
-
-musical_artist_age = [
-    {'ontology/birthYear': '1998',
-    'ontology/deathYear': '2015'},
-    {'ontology/birthYear': '1880',
-    'ontology/deathYear': '1999'
-    }
-]
-
-with open('musical_artist_age.csv', 'w', encoding= 'utf8') as file: 
-    file.write('birthYear, deathYear, \n')
-    for entry in musical_artist_age: 
-            file.write(f"{entry['ontology/birthYear']}, {entry['ontology/deathYear']}\n")
+with open('musical_artist.csv', 'w', encoding= 'utf8') as file: 
+    file.write('birthYear, deathYear \n')
+    for entry in musical_artist: 
+        if 'ontology/birthYear' in entry and 'ontology/deathYear' in entry:
+            file.write(f'{entry["ontology/birthYear"]}, {entry["ontology/deathYear"]}\n')
+        
